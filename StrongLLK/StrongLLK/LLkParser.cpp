0fc5829649933726b4a92734e4fa1968c3287c89
donydony2009 <donydony2009@gmail.com>
2016-04-18 19:15:41 +0300

    Some more progress

--- StrongLLK/StrongLLK/LLkParser.cpp
+++ StrongLLK/StrongLLK/LLkParser.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "LLkParser.h"
+#include <assert.h> 
 
 LLkParser::LLkParser(std::string filename)
 {
@@ -12,4 +13,62 @@ LLkParser::LLkParser(std::string filename)
         f>>state>>value;
         m_States[state].push_back(value);
     }
+}
+
+std::vector<std::string> LLkParser::First(const std::string& val)
+{
+	std::vector<std::string> result;
+	if(val[0] == '~')
+	{
+		assert(val.size() == 1);
+		result.push_back(val);
+		return result;
+	}
+	for(char c : val)
+	{
+		if(IsNonTerminal(c))
+		{
+			std::string temp;
+			temp = c;
+			AppendToAll(result, First(temp));
+		}
+		else
+		{
+			std::string temp;
+			temp = c;
+			AppendToAll(result, temp);
+		}
+	
+	}
+	if(val.size() == 1 && IsNonTerminal(val[0]))
+	{
+		m_FirstCache[val[0]] = result;
+	}
+	return result;
+}
+
+bool LLkParser::IsNonTerminal(char c)
+{
+	return c >= 'A' && c <= 'Z';
+}
+
+void LLkParser::AppendToAll(std::vector<std::string>& appendTo, std::string appended)
+{
+	for(auto& str : appendTo)
+	{
+		str += appended;
+	}
+}
+
+void LLkParser::AppendToAll(std::vector<std::string>& appendTo, const std::vector<std::string>& appended)
+{
+	std::vector<std::string>& old(appendTo);
+	appendTo.clear();
+	for(auto& str1 : old)
+	{
+		for(auto& str2 : appended)
+		{
+			appendTo.push_back(str1 + str2);
+		}
+	}
 }
--- StrongLLK/StrongLLK/LLkParser.h
+++ StrongLLK/StrongLLK/LLkParser.h
@@ -1,14 +1,22 @@
 #include <string>
-#include <unordered_map>
 #include <vector>
+#include <unordered_map>
+
 #include <fstream>
 
 typedef unsigned int u32;
 class LLkParser
 {
-    u32 m_K;
-    std::unordered_map<char, std::vector<std::string>> m_States;
 public:
     LLkParser(std::string filename);
+	std::vector<std::string> First(const std::string& val);
+private:
+	bool IsNonTerminal(char c);
+	void AppendToAll(std::vector<std::string>& appendTo, std::string appended);
+	void AppendToAll(std::vector<std::string>& appendTo, const std::vector<std::string>& appended);
 
+	u32 m_K;
+	std::unordered_map<char, std::vector<std::string>> m_States;
+	std::unordered_map<char, std::vector<std::string>> m_FirstCache;
+	std::unordered_map<char, std::vector<std::string>> m_FollowCache;
 };
