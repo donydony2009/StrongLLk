bd1dc79439381a85b6f89e8c83c7540c918e1f8d
Dony <donydony2009@gmail.com>
2016-04-19 08:43:25 +0300

    New Changes

--- StrongLLK/StrongLLK/LLkParser.cpp
+++ StrongLLK/StrongLLK/LLkParser.cpp
@@ -13,43 +13,107 @@ LLkParser::LLkParser(std::string filename)
         f>>state>>value;
         m_States[state].push_back(value);
     }
+
+	for(auto& state : m_States)
+	{
+		std::string tmp;
+		tmp = state.first;
+		First(tmp);
+	}
+	int i = 0;
+	i++;
+
+	i--;
 }
 
 std::vector<std::string> LLkParser::First(const std::string& val)
 {
 	std::vector<std::string> result;
-	if(val[0] == '~')
-	{
-		assert(val.size() == 1);
-		result.push_back(val);
-		return result;
-	}
-	for(char c : val)
-	{
-		if(IsNonTerminal(c))
-		{
-			std::string temp;
-			temp = c;
-			AppendToAll(result, First(temp));
-		}
-		else
-		{
-			std::string temp;
-			temp = c;
-			AppendToAll(result, temp);
-		}
 	
-	}
 	if(val.size() == 1 && IsNonTerminal(val[0]))
 	{
+		auto& it = m_FirstCache.find(val[0]);
+		if(it != m_FirstCache.end())
+		{
+			return it->second;
+		}
+
+		for(auto& state : m_States[val[0]])
+		{
+			auto& stateFirsts = First(state);
+			for(auto& stateFirst : stateFirsts)
+			{
+				if(std::find(result.begin(), result.end(), stateFirst) == result.end())
+				{
+					result.push_back(stateFirst);
+				}
+			}
+		}
 		m_FirstCache[val[0]] = result;
 	}
+	else
+	{
+		if(val[0] == '~')
+		{
+			assert(val.size() == 1);
+			return result;
+		}
+
+		std::vector<std::string> intermediateResult;
+		intermediateResult.push_back("");
+		for(char c : val)
+		{
+			if(IsNonTerminal(c))
+			{
+				std::string temp;
+				temp = c;
+				AppendToAll(intermediateResult, First(temp));
+			}
+			else
+			{
+				std::string temp;
+				temp = c;
+				AppendToAll(intermediateResult, temp);
+			}
+			MoveIntermediateResults(intermediateResult, result);
+			if(intermediateResult.size() == 0)
+				break;
+		}
+		result.insert(result.end(), intermediateResult.begin(), intermediateResult.end());
+	}
 	return result;
+}
+
+std::vector<std::string> LLkParser::Follow(char val, std::vector<char>& callstack)
+{
+
 }
 
 bool LLkParser::IsNonTerminal(char c)
 {
 	return c >= 'A' && c <= 'Z';
+}
+
+void LLkParser::MoveIntermediateResults(std::vector<std::string>& intermediateResult, std::vector<std::string>& result)
+{
+	int size = intermediateResult.size();
+	for(int i = 0; i < size; i++)
+	{
+		if(intermediateResult[i].size() >= m_K)
+		{
+			intermediateResult[i].resize(m_K);
+			if(std::find(result.begin(), result.end(), intermediateResult[i]) == result.end())
+			{
+				result.push_back(intermediateResult[i]);
+			}
+			if(size != i + 1)
+			{
+				std::move(intermediateResult.begin() + i + 1, intermediateResult.end(), intermediateResult.begin() + i);
+			}
+			size--;
+		}
+	}
+	intermediateResult.resize(size);
 }
 
 void LLkParser::AppendToAll(std::vector<std::string>& appendTo, std::string appended)
@@ -62,13 +126,18 @@ void LLkParser::AppendToAll(std::vector<std::string>& appendTo, std::string appe
 
 void LLkParser::AppendToAll(std::vector<std::string>& appendTo, const std::vector<std::string>& appended)
 {
-	std::vector<std::string>& old(appendTo);
+	std::vector<std::string> old;
+	old.insert(old.end(), appendTo.begin(), appendTo.end());
 	appendTo.clear();
 	for(auto& str1 : old)
 	{
 		for(auto& str2 : appended)
 		{
-			appendTo.push_back(str1 + str2);
+			std::string toAdd = str1 + str2;
+			if(std::find(appendTo.begin(), appendTo.end(), toAdd) == appendTo.end())
+			{
+				appendTo.push_back(str1 + str2);
+			}
 		}
 	}
 }
--- StrongLLK/StrongLLK/LLkParser.h
+++ StrongLLK/StrongLLK/LLkParser.h
@@ -10,12 +10,16 @@ class LLkParser
 public:
     LLkParser(std::string filename);
 	std::vector<std::string> First(const std::string& val);
+	std::vector<std::string> Follow(char val, std::vector<char>& callstack = std::vector<char>());
 private:
 	bool IsNonTerminal(char c);
+	void MoveIntermediateResults(std::vector<std::string>& intermediateResult, std::vector<std::string>& result);
 	void AppendToAll(std::vector<std::string>& appendTo, std::string appended);
 	void AppendToAll(std::vector<std::string>& appendTo, const std::vector<std::string>& appended);
+	bool CheckLengthRequirement(std::vector<std::string> toCheck);
 
 	u32 m_K;
+	std::string m_Error;
 	std::unordered_map<char, std::vector<std::string>> m_States;
 	std::unordered_map<char, std::vector<std::string>> m_FirstCache;
 	std::unordered_map<char, std::vector<std::string>> m_FollowCache;
